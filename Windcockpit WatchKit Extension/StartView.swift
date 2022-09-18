590155301e0e45a7a86dbf5d64eac7768834cfbc
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2022-09-18 11:12:47 +0200

    Adds wind and kitesurfing

--- Windcockpit WatchKit Extension/StartView.swift
+++ Windcockpit WatchKit Extension/StartView.swift
@@ -11,20 +11,37 @@ import HealthKit
 struct StartView: View {
     @EnvironmentObject var workoutManager: WorkoutManager
     
-    var workoutTypes: [HKWorkoutActivityType] = [.cycling, .sailing]
+    var workoutTypes: [HKWorkoutActivityType] = [.cycling, .sailing, .surfingSports]
     
     var body: some View {
-        NavigationLink(
-            "Start session",
-            destination: SessionPagingView(),
-            tag: .cycling,
-            selection: $workoutManager.selectedWorkout
-        )
+        List(workoutTypes) { sessionType in
+            VStack {
+                ZStack {
+                    Circle()
+                        .stroke(.green, lineWidth: 1)
+                    Text(short(name: sessionType.name))
+                        .font(.system(size: 20))
+                }
+                .frame(width: 40, height: 40)
+                .padding(.leading, -50)
+                
+                NavigationLink(
+                    sessionType.name,
+                    destination: SessionPagingView(),
+                    tag: sessionType,
+                    selection: $workoutManager.selectedWorkout
+                )
+            }
+            .padding(
+                EdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            )
+        }
+        .listStyle(.carousel)
+        .navigationTitle("Windcockpit")
         .onAppear {
             workoutManager.requestAuthorization()
             workoutManager.requestPermission()
         }
-        .navigationTitle("Windcockpit")
     }
 }
 
@@ -43,8 +60,21 @@ extension HKWorkoutActivityType: Identifiable {
         switch self {
         case .cycling:
             return "Wingfoiling"
+        case .sailing:
+            return "Windsurfing"
+        case .surfingSports:
+            return "Kitesurfing"
         default:
             return ""
         }
+    }
+}
+
+func short(name: String) -> String {
+    switch name {
+    case "Windsurfing": return "WS"
+    case "Wingfoiling": return "WF"
+    case "Kitesurfing": return "KS"
+    default: return name
     }
 }
